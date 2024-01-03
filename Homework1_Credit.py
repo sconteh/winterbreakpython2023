71c1351ac374f20a416fede975df3330271848bb
sconteh <153090727+sconteh@users.noreply.github.com>
2024-01-02 23:36:31 -0500

    Update Homework1_Credit.py

--- Homework1_Credit.py
+++ Homework1_Credit.py
@@ -16,34 +16,46 @@ class User():
     def authenticate(self, entered_password):
         return entered_password == self.password
     
-    def deposit(self, amount):
-        entered_password = input("Enter your password: ")
-        if self.authenticate(entered_password):
-            amount = float(input("Enter the amount to deposit: "))
-            self.balance += amount
-            print(f"Deposit successful!\nCurrent Balance: {self.balance}" )
-        else: 
-            print("Unable to deposit: Incorrect password.")
+    def change_password(self, old_password, new_password):
+        if self.authenticate(old_password):
+            self.password = new_password
+            print("Password changed successfully!")
+        else:
+            print("Unable to change password: Incorrect old password.")
 
-    def withdrawal(self, amount):
-        entered_password = input("Enter your password: ")
-        if self.authenticate(entered_password):
-            amount = float(input("Enter the amount to withdraw: "))
-            if amount <= self.balance:
-                self.balance -= amount 
-                print(f"Withdrawal successful!\nCurrent Balance: {self.balance}")
-            else:
-                print("Cannot withdraw amount requested.")
-        else: 
-            print("Unable to deposit: Incorrect password.")
-        
+    def deposit(self):
+        amount = float(input("Enter the amount to deposit: "))
+        self.balance += amount
+        print(f"Deposit successful!\nCurrent Balance: {self.balance}" ) 
+
     def display_details(self):
+        print("\n")
         print("Personal Details of User")
         print("Name:", self.name)
         print("Age:", self.age)
         print("Gender:", self.gender)
         print("Balance:", self.balance)
 
+    def withdrawal(self):
+        amount = float(input("Enter the amount to withdraw: "))
+        if amount <= self.balance:
+            self.balance -= amount 
+            print(f"Withdrawal successful!\nCurrent Balance: {self.balance}")
+        else:
+            print("Cannot withdraw amount requested.")
+
+def access_user(users):
+    print("Enter your username and password for the account you would like to access: ")
+    entered_username = input("Username: ")
+    entered_password = input("Password: ")
+
+    for user in users:
+        if user.username == entered_username and user.authenticate(entered_password):
+            return user
+            
+    print("Invalid username or password. Please try again.")
+    return None 
+    
 def create_account():
     # Getting User Details
     print("Before we can create a bank account, you must first create a username and password associat1e with it.\n")
@@ -57,50 +69,81 @@ def create_account():
     user = User(username, password, name, age, gender) # creation of the instance of the user class  
     print("\nAccount Created!\n")
     user.display_details() # displaying the user information 
-    return user # returns the created user 
+    return user # returns the created use1r 
 
 
 def main():
-    print("*********************************")
+    print("\n*********************************")
     print("* WELCOME TO INST BANK DATABASE *")
     print("*********************************")
 
-    user = None  # Initialize user outside the loop
+    users = [] # list to store User instances  
 
     while True:
-        print("Options\n")
-        print("1. Create a New Account\n")
-        print("2. Deposit\n")
-        print("3. Withdraw\n")
-        print("4. Display Account Information\n")
-        print("5. Exit\n")
+        print("\nOptions")
+        print("1. Create a New Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Display Account Information")
+        print("5. Change Password")
+        print("6. Exit\n")
 
         choice = input("Enter your choice (1-5): ")
 
-        if choice == '1': 
+        if choice == '1': # creating an account 
             user = create_account()
+            users.append(user)
         
-        elif choice == '2': 
-            if user: 
-                 user.deposit()
+        elif choice == '2': # deposit
+
+            if users: 
+                selected_user = access_user(users)
+                if selected_user:
+                     selected_user.deposit()
+                else:
+                    print("No matching account found.")
             else: 
-                print("\n--------------------------------------------------------------------------------------")
-                print("\nNo accounts found. An account must be created first before depositing.")            
-        elif choice == '3': 
-            if user: 
-                user.withdrawal()
+                print("\n-----------------------------------------------------------------------")
+                print("No accounts found. An account must be created first before depositing")
+                print("-----------------------------------------------------------------------")
+
+        elif choice == '3': # withdrawal
+            if users:   
+                selected_user = access_user(users)
+                if selected_user:
+                     selected_user.withdrawal()
+                else:
+                    print("No matching account found.")
             else:
-                print("\n--------------------------------------------------------------------------------------")
-                print("\nNo accounts found. An account must be created first before withdrawing.\n")
+                print("\n------------------------------------------------------------------------")
+                print("No accounts found. An account must be created first before withdrawing")
+                print("------------------------------------------------------------------------")
 
-        elif choice == '4':
-            if user:
-                user.display_details()
+        elif choice == '4': # displaying bank information 
+            if users:
+                selected_user = access_user(users)
+                if selected_user:
+                    selected_user.display_details()
             else: 
-                print("\n--------------------------------------------------------------------------------------")
-                print("No accounts found. An account must be created first before displaying an account.\n")
+                print("\n--------------------------------------------------------------------------------")
+                print("No accounts found. An account must be created first before displaying an account")
+                print("--------------------------------------------------------------------------------")
 
-        elif choice == '5': 
+        elif choice == '5': # changing password 
+            if users:
+                selected_user = access_user(users)
+                if selected_user:
+                    old_password = input("Enter your old password: ")
+                    new_password = input("Enter your new password: ")
+                    selected_user.change_password(old_password, new_password)
+                else:
+                    print("No matching account found. Please try again.")
+            else:
+                print("\n--------------------------------------------------------------------------------")
+                print("No accounts found. An account must be created first before changing your password")
+                print("---------------------------------------------------------------------------------")
+
+        elif choice == '6': # exiting program
             print("Exiting program...")
             break
 
